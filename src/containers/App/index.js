ac9a6588a5b9bfb5c02a36904720355e16f29791
RyanDBarnett <conner2468@gmail.com>
2019-06-04 15:09:58 -0600

    Import Form into App

--- src/containers/App/index.js
+++ src/containers/App/index.js
@@ -3,17 +3,19 @@ import { connect } from 'react-redux'
 import './App.css';
 import fetchSightings from '../../thunks/fetchSightings/';
 import PropTypes from 'prop-types';
+import Form from '../Form';
 
 class App extends Component {
 
   componentDidMount = () => {
-    const url = 'https://ebird.org/ws2.0/data/obs/US-CO/recent'
+    const url = 'https://ebird.org/ws2.0/data/obs/US-CO/recent';
     this.props.fetchSightings(url);
   }
 
   render() {
     return (
       <div className="App">
+        <Form />
         <h1>Hello!</h1>
       </div>
     );
