8a73320aac47d32e6e205a0bb3e9a3df18c6a600
RyanDBarnett <conner2468@gmail.com>
2019-06-04 10:14:23 -0600

    Setup thunk/composeWithDevTools/applyMiddleware in src/index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { render } from 'react-dom';
 import App from './containers/App/';
-
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import rootReducer from '../src/reducers';
+import { createStore, applyMiddleware } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import thunk from 'redux-thunk';
 
-const store = createStore(rootReducer);
+const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)));
 
 render(
   <Provider store={ store } >
