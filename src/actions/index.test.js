1d8bd62fc6c194319432d9020f26275ab2ec1ddd
RyanDBarnett <conner2468@gmail.com>
2019-06-04 11:35:30 -0600

    Test setSighting, isLoading, and hasErrored actions

---
+++ src/actions/index.test.js
@@ -0,0 +1,39 @@
+import * as actions from './';
+
+describe('actions', () => {
+  it('should have a type of SET_SIGHTINGS', () => {
+    const sightings = ['bird1', 'bird2'];
+    const expectedAction = {
+      type: 'SET_SIGHTINGS',
+      sightings
+    };
+
+    const result = actions.setSightings(sightings)
+
+    expect(result).toEqual(expectedAction)
+  });
+
+  it('should have a type of IS_LOADING', () => {
+    const bool = true;
+    const expectedAction = {
+      type: 'IS_LOADING',
+      isLoading: bool
+    };
+
+    const result = actions.isLoading(bool)
+
+    expect(result).toEqual(expectedAction)
+  });
+
+  it('should have a type of HAS_ERRORED', () => {
+    const message = 'Error Message';
+    const expectedAction = {
+      type: 'HAS_ERRORED',
+      message
+    };
+
+    const result = actions.hasErrored(message)
+
+    expect(result).toEqual(expectedAction)
+  });
+});
