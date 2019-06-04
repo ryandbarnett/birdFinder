8b64245e7a0784c4dc39a16f6f0a33e871128a27
RyanDBarnett <conner2468@gmail.com>
2019-06-04 16:27:02 -0600

    Add mapStateToProps, handleChange method, state for species input value to Form

--- src/containers/Form/index.js
+++ src/containers/Form/index.js
@@ -2,42 +2,50 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux'
 import fetchSightings from '../../thunks/fetchSightings/';
 import { names } from '../../utils/data.js';
-import { EBIRD_API_KEY } from '../../utils/apiKeys.js';
 import TextInput from 'react-autocomplete-input';
 import 'react-autocomplete-input/dist/bundle.css';
 
 class Form extends Component {
-  handleSubmit = async (e) => {
-    let speciesCode;
-    e.preventDefault();
-    const inputs = Array.from(e.target.elements).reduce((acc, element) => {
-      if (element.name) {
-        acc[element.name] = element.value.trim();
+  constructor() {
+    super()
+    this.state = {
+      species: {
+        value: ''
       }
-      return acc;
-    }, {});
-
-    if (names.includes(inputs.species)) {
-      speciesCode = this.state.sightings.reduce((acc, sighting) => {
-        const {comName, sciName} = sighting;
-        if (comName === inputs.species || sciName === inputs.species) {
-          acc = sighting.speciesCode;
-        }
-        return acc;
-      }, '')
     }
-    const baseUrl = 'https://ebird.org/ws2.0/data/obs/US-CO/recent/'
-    const url = baseUrl + speciesCode + '?maxResults=200';
+  }
+
+  handleSubmit = async (e) => {
+    e.preventDefault();
+    let { value } = this.state.species;
+    value = value.trim();
+    let speciesCode;
+    if (names.includes(value)) {
+      speciesCode = this.props.sightings.find(sighting => {
+        const {comName, sciName} = sighting;
+        return comName === value || sciName === value;
+      }).speciesCode;
+    }
+
+    const baseUrl = 'https://ebird.org/ws2.0/data/obs/US-CO/recent';
+    const max = '100';
+    const url = !speciesCode ? `${baseUrl}${max}` : `${baseUrl}/${speciesCode}?maxResults=${max}`;
     this.props.fetchSightings(url);
+  }
+
+  handleChange = (e) => {
+    this.setState({species: {value: e}});
   }
   
   render() {
     return (
       <form onSubmit={this.handleSubmit}>
         <TextInput 
+          onChange={this.handleChange}
           name='species'
           Component='input' 
-          placeholder='Species' 
+          placeholder='Species'
+          value={this.state.species.value} 
           id='species-input' 
           matchAny={true} 
           trigger={''} 
@@ -49,10 +57,16 @@ class Form extends Component {
   }
 }
 
+export const mapStateToProps = (state) => ({ 
+  sightings: state.sightings,
+  isLoading: state.isLoading,
+  error: state.error
+});
+
 export const mapDispatchToProps=(dispatch) => ({
   fetchSightings: (url) => {
     dispatch(fetchSightings(url))
   }
 });
 
-export default connect(null, mapDispatchToProps)(Form);
+export default connect(mapStateToProps, mapDispatchToProps)(Form);
