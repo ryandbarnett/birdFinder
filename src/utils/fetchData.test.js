948a94a315682fdfc6d2de48a28659239e32e99b
RyanDBarnett <conner2468@gmail.com>
2019-06-04 15:01:03 -0600

    Test fetchData util function

---
+++ src/utils/fetchData.test.js
@@ -0,0 +1,52 @@
+import fetchData from './fetchData.js';
+import { EBIRD_API_KEY } from './apiKeys.js';
+
+describe('fetchData', () => {
+  let mockUrl;
+  let mockResponse;
+
+  beforeEach(() => {
+    mockUrl = 'https://ebird.org/ws2.0/data/obs/US-CO/recent';
+    mockResponse = ['bird1', 'bird2'];
+
+    window.fetch = jest.fn().mockImplementation(() => {
+      return Promise.resolve({
+        ok: true,
+        json: () => Promise.resolve(mockResponse)
+      });
+    });
+  });
+
+  it('should be called with correct params', () => {
+    const expected = [
+      mockUrl,
+      {
+        method: 'GET',
+        headers: {
+          'x-ebirdapitoken': EBIRD_API_KEY
+        }
+      }
+    ];
+
+    fetchData(mockUrl);
+
+    expect(window.fetch).toHaveBeenCalledWith(...expected);
+  });
+
+  it('should return a parsed response if status is ok', async () => {
+    const result = await fetchData(mockUrl);
+
+    expect(result).toEqual(mockResponse);
+  });
+
+  it('should return an error if status is not ok', async () => {
+    window.fetch = jest.fn().mockImplementation(() => {
+      return Promise.resolve({
+        ok: false,
+        statusText: 'Error Message'
+      });
+    });
+
+    await expect(fetchData()).rejects.toEqual(Error('Error Message'))
+  });
+});
