542e51a5f2a8e2bc66afad7c8b30591ce74ae9ea
RyanDBarnett <conner2468@gmail.com>
2019-06-04 10:02:42 -0600

    Create isLoading and hasErrored actions

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,14 @@
-export const setSightings = (sightings) => {
-  return {
-    type: 'SET_SIGHTINGS',
-    sightings
-  }
-}
+export const setSightings = (sightings) => ({
+  type: 'SET_SIGHTINGS',
+  sightings
+});
+
+export const isLoading = (bool) => ({
+  type: 'IS_LOADING',
+  isLoading: bool
+});
+  
+export const hasErrored = (message) => ({
+  type: 'HAS_ERRORED',
+  message
+});
