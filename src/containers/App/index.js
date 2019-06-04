0d7105a23d470334c945f4e9a3101a34fb742a1b
RyanDBarnett <conner2468@gmail.com>
2019-06-04 09:29:44 -0600

    Make App a component and fetch sightings on componentDidMount

--- src/containers/App/index.js
+++ src/containers/App/index.js
@@ -1,12 +1,36 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
+import { EBIRD_API_KEY } from '../../utils/apiKeys.js';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hello!</h1>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      sightings: []
+    }
+  }
+
+  componentDidMount = async () => {
+    const options = {
+      method: 'GET',
+      headers: {
+        'x-ebirdapitoken': EBIRD_API_KEY
+      }
+    }
+    const url = 'https://ebird.org/ws2.0/data/obs/US-CO/recent'
+
+    const response = await fetch(url, options);
+    const sightings = await response.json();
+    this.setState({sightings})
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Hello!</h1>
+      </div>
+    );
+  }
 }
 
 export default App;
