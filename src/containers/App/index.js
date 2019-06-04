216124ecb92f2a52b4850b5c3f44a12ad87f8891
RyanDBarnett <conner2468@gmail.com>
2019-06-04 11:17:48 -0600

    Import PropTypes into App

--- src/containers/App/index.js
+++ src/containers/App/index.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux'
 import './App.css';
 import fetchSightings from '../../thunks/fetchSightings/';
+import PropTypes from 'prop-types';
 
 class App extends Component {
 
