7d1bf39fb7320b85525dcb53aa19188c4fa76ee3
RyanDBarnett <conner2468@gmail.com>
2019-06-04 18:43:08 -0600

    Fix linter errors in reducers/index.js

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,12 +1,12 @@
 import { combineReducers } from 'redux';
-import sightings from './sightingsReducer/';
-import hasErrored from './hasErroredReducer/';
-import isLoading from './isLoadingReducer/';
+import sightings from './sightingsReducer';
+import hasErrored from './hasErroredReducer';
+import isLoading from './isLoadingReducer';
 
 const rootReducer = combineReducers({
   sightings,
   isLoading,
-  error: hasErrored
-})
+  error: hasErrored,
+});
 
 export default rootReducer;
