de3d4ffa62b2c77d38f5d25eeb1655857aab0864
RyanDBarnett <conner2468@gmail.com>
2019-06-05 10:50:37 -0600

    Test mapDispatch and mapStateToProps in App

--- src/containers/App/index.test.js
+++ src/containers/App/index.test.js
@@ -1,7 +1,9 @@
 import React from 'react'; 
 import { shallow } from 'enzyme';
-import { App } from '.';
+import { App, mapStateToProps, mapDispatchToProps } from '.';
+import fetchSightings from '../../thunks/fetchSightings';
 
+jest.mock('../../thunks/fetchSightings');
 
 describe('App', () => {
   let wrapper;
@@ -22,5 +24,38 @@ describe('App', () => {
     wrapper = shallow( <App fetchSightings={fetchSightings} error={'Error'} />);
 
     expect(wrapper).toMatchSnapshot(); 
-  }); 
+  });
+
+  describe('mapStateToProps', () => {
+    it('should return an object with error, sightings, and isLoading', () => {
+      const mockState = {
+        sightings: ['bird1', 'bird2'],
+        error: '',
+        isLoading: false,
+        kitties: 'Meow meow',
+      };
+      const expected = {
+        sightings: ['bird1', 'bird2'],
+        error: '',
+        isLoading: false,
+      };
+
+      const mappedProps = mapStateToProps(mockState);
+
+      expect(mappedProps).toEqual(expected);
+    });
+  });
+
+  describe('mapDispatchToProps', () => {
+    it('calls dispatch with a fetchSightings action when componentDidMount is called', () => {
+      const mockDispatch = jest.fn();
+      const mockUrl = 'https://testUrl.com';
+      const actionToDispatch = fetchSightings(mockUrl);
+
+      const mappedProps = mapDispatchToProps(mockDispatch)
+      mappedProps.fetchSightings(mockUrl)
+
+      expect(mockDispatch).toHaveBeenCalledWith(actionToDispatch)
+    });
+  });
 });
