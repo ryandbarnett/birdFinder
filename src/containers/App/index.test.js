434568ff8830e02489de67241764f0535d69a1d2
RyanDBarnett <conner2468@gmail.com>
2019-06-05 10:36:54 -0600

    Create dynamic snapshot tests for App

--- src/containers/App/index.test.js
+++ src/containers/App/index.test.js
@@ -4,9 +4,22 @@ import { App } from '.';
 
 
 describe('App', () => {
+  let wrapper;
   const fetchSightings = jest.fn(); 
   it('should match the snapshot', () => { 
-    const wrapper = shallow( <App fetchSightings={fetchSightings} />);
+    wrapper = shallow( <App fetchSightings={fetchSightings} />);
+
+    expect(wrapper).toMatchSnapshot(); 
+  });
+
+  it('should have match snapshot if isLoading is true', () => { const
+    wrapper = shallow( <App fetchSightings={fetchSightings} isLoading={true} />);
+
+    expect(wrapper).toMatchSnapshot(); 
+  });
+
+  it('should have match snapshot if there is an error', () => { const
+    wrapper = shallow( <App fetchSightings={fetchSightings} error={'Error'} />);
 
     expect(wrapper).toMatchSnapshot(); 
   }); 
