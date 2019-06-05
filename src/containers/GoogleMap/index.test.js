f2a82114fc498a5af66a658220690a938d5607f5
RyanDBarnett <conner2468@gmail.com>
2019-06-05 10:52:23 -0600

    Test mapStateToProps in GoogleMap

--- src/containers/GoogleMap/index.test.js
+++ src/containers/GoogleMap/index.test.js
@@ -1,6 +1,6 @@
 import React from 'react'; 
 import { shallow } from 'enzyme';
-import { GoogleMap } from '.';
+import { GoogleMap, mapStateToProps } from '.';
 
 
 describe('GoogleMap', () => { 
@@ -8,5 +8,25 @@ describe('GoogleMap', () => {
     const wrapper = shallow( <GoogleMap />);
 
     expect(wrapper).toMatchSnapshot(); 
-  }); 
+  });
+
+  describe('mapStateToProps', () => {
+    it('should return an object with error, sightings, and isLoading', () => {
+      const mockState = {
+        sightings: ['bird1', 'bird2'],
+        error: '',
+        isLoading: false,
+        kitties: 'Meow meow',
+      };
+      const expected = {
+        sightings: ['bird1', 'bird2'],
+        error: '',
+        isLoading: false,
+      };
+
+      const mappedProps = mapStateToProps(mockState);
+
+      expect(mappedProps).toEqual(expected);
+    });
+  });
 });
